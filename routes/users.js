e0ee5220091a59cf430db9c0162abf5bb728c54e
Adarshsathyan <adarshsathyan2000@gmail.com>
2020-11-25 11:06:31 +0530

    Separated user and admin layout

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user/login',{admin:false});
+  res.render('user/login',{layout:'./layoutuser'});
 });
 
 module.exports = router;
